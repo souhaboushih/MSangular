c61955b58ada142d1eff121e3ba686b871039de9
IssaniRania <raniaissani83@gmail.com>
2023-12-16 22:31:23 +0100

    crud classe

---
+++ src/app/sevices/classe.service.ts
@@ -0,0 +1,33 @@
+// classe.service.ts
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RefreshService } from './refresh.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClasseService {
+  private apiUrl = 'http://localhost:3000/classes'; // Remplacez par l'URL de votre API
+
+  constructor(private http: HttpClient, private refreshService: RefreshService) { }
+
+  getClasses(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+  addClasse(nouvelleClasse: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, nouvelleClasse);
+  }
+  updateClasse(id: string, classeModifiee: any): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.put<any>(url, classeModifiee);
+  }
+  deleteClasse(id: string): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete(url);
+  }
+  emitRefresh(): void {
+    this.refreshService.emitRefresh();
+  }
+}
