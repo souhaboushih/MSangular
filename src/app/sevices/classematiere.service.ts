0ba0475450b36a1d7a24d625c369e371bf8e1a24
firasabidli <abidlifiras@bizerte.r-iset.tn>
2023-12-22 15:50:39 +0100

    utilise le classe comme clé étrangère au niveau de l'ajout de l'élève et l'update

---
+++ src/app/sevices/classematiere.service.ts
@@ -0,0 +1,24 @@
+// classe.service.ts
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RefreshService } from './refresh.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClassematiereService {
+  private apiUrl = 'http://localhost:3000'; // Remplacez par l'URL de votre API
+
+  constructor(private http: HttpClient, private refreshService: RefreshService) { }
+
+  
+  getClasses(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/classes`);
+  }
+ 
+  emitRefresh(): void {
+    this.refreshService.emitRefresh();
+  }
+}
